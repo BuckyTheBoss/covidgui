c44f9e67c0d54fa7b14af25f4789cace9f88c75a
Avi <buchman.avi@gmail.com>
2020-04-06 19:29:24 +0300

    changing to write binary 2

--- main/views.py
+++ main/views.py
@@ -38,7 +38,7 @@ def export_file(covid_id):
     response['Content-Disposition'] = f'attachment; filename={filename}'
    
     os.chdir(settings.EXPORT_STRING)
-    with open(filename, 'wb') as f:
+    with open(filename, 'w', encoding='windows-1255') as f:
         f.write(content_1255)
         f.close()
         
